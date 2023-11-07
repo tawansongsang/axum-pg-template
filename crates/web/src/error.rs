3082bdff989c5bd6b3ce89b2b6aac624baf34633
Tawansongsang Karnkawinpong <tawansongsang.k@ipthailand.go.th>
2023-11-07 07:57:22 +0000

    after trick serialize struct

--- crates/web/src/error.rs
+++ crates/web/src/error.rs
@@ -7,6 +7,7 @@ use serde::Serialize;
 pub type Result<T> = core::result::Result<T, Error>;
 
 #[derive(Clone, Debug, Serialize, strum_macros::AsRefStr)]
+#[serde(tag = "type", content = "data")]
 pub enum Error {
     LoginFail,
 
