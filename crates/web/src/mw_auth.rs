3ca00b17d36afe1d31541376789df1c9d76ed3a9
Tawansongsang Karnkawinpong <tawansongsang.k@ipthailand.go.th>
2023-11-07 02:55:46 +0000

    change ctx extractor to middleware

--- crates/web/src/mw_auth.rs
+++ crates/web/src/mw_auth.rs
@@ -1,5 +1,5 @@
 use async_trait::async_trait;
-use axum::extract::FromRequestParts;
+use axum::extract::{FromRequestParts, State};
 use axum::http::request::Parts;
 use axum::http::Request;
 use axum::middleware::Next;
@@ -10,6 +10,7 @@ use tower_cookies::Cookies;
 
 use crate::ctx::Ctx;
 use crate::error::{Error, Result};
+use crate::model::ModelController;
 use crate::routes::AUTH_TOKEN;
 
 pub async fn mw_require_auth<B>(
@@ -17,16 +18,36 @@ pub async fn mw_require_auth<B>(
     req: Request<B>,
     next: Next<B>,
 ) -> Result<Response> {
-    println!("--> {:<12} - mw_require_auth", "HANDLER");
+    println!("--> {:<12} - mw_require_auth - {ctx:?}", "HANDLER");
+
+    ctx?;
+
+    // TODO: Token components validation.
+
+    Ok(next.run(req).await)
+}
+
+pub async fn mw_ctx_resolver<B>(
+    _mc: State<ModelController>,
+    cookies: Cookies,
+    mut req: Request<B>,
+    next: Next<B>,
+) -> Result<Response> {
+    println!("->> {:<12} - mw_ctx_resolver", "MIDDLEWARE");
 
     let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
 
-    // Parse token.
-    let (user_id, exp, sign) = auth_token
+    // Compute Result<Ctx>
+    let result_ctx = match auth_token
         .ok_or(Error::AuthFailNoAuthTokenCookie)
-        .and_then(parse_token)?;
-
-    // TODO: Token components validation.
+        .and_then(parse_token)
+    {
+        Ok((user_id, _exp, _sign)) => {
+            // TODO: Token components validations.
+            Ok(Ctx::new(user_id))
+        }
+        Err(e) => Err(e),
+    };
 
     Ok(next.run(req).await)
 }
@@ -37,7 +58,7 @@ impl<S: Send + Sync> FromRequestParts<S> for Ctx {
     type Rejection = Error;
 
     async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self> {
-        println!("--> {:<12} -- Ctx", "EXTRACTOR");
+        println!("->> {:<12} -- Ctx", "EXTRACTOR");
 
         // User the cookies extractor.
         let cookies = parts.extract::<Cookies>().await.unwrap();
