a6a756c47af04f8e6cd5b9733e1e8c03c809f3c1
Tawansongsang Karnkawinpong <tawansongsang.k@ipthailand.go.th>
2023-11-09 03:43:11 +0000

    setup postgres dev env

--- crates/web/src/main.rs
+++ crates/web/src/main.rs
@@ -7,6 +7,9 @@ mod log;
 mod model;
 mod routes;
 
+// #[cfg(test)] // Commented during early development.
+pub mod _dev_utils;
+
 pub use self::error::{Error, Result};
 pub use config::config; // can use crate::config
 
@@ -31,6 +34,9 @@ async fn main() -> Result<()> {
         .with_target(false) // can set true if you want
         .with_env_filter(EnvFilter::from_default_env())
         .init();
+
+    // -- FOR DEV ONLY
+    _dev_utils::init_dev().await;
 
     // Initialize ModelController.
     let mm = ModelManager::new().await?;
